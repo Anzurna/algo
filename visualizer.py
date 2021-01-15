dcd44ae70b023ca7cfa366fead517f6fed6f58a8
Anzurna <aversatrix@mail.ru>
2021-01-15 13:43:19 +0300

    refactoring fixes

--- visualizer.py
+++ visualizer.py
@@ -5,14 +5,14 @@ from a_star_np import algo as compute_a_star
 
 class AlgoVisualizer:
     def __init__(self):
-		self.height = 600
+        self.height = 600
         self.width = 800
 
         self.DISPLAYSURF = pygame.display.set_mode((self.width, self.height))
         self.rectangles = []
         self.block_size = 5
         self.border_size = 1
-        self.general_info = self.new_gen_info()
+        self.general_info = self.gen_new_info()
 
         self.COLOR_BLUE = (0, 0, 255)
         self.COLOR_RED = (255, 0, 0)
@@ -32,28 +32,31 @@ class AlgoVisualizer:
 
     def create_grid(self) -> None:
         for y in range(0, self.height, self.block_size + self.border_size):
-            self.r_row = []
+            r_row = []
             for x in range(0, self.width, self.block_size + self.border_size):
                 rect = pygame.Rect(x, y, self.block_size, self.block_size)
-                self.r_row.append([rect, self.COLOR_GREEN, 0, 0])
-            self.rectangles.append(self.r_row)
+                r_row.append([rect, self.COLOR_GREEN])
+            self.rectangles.append(r_row)
 
     def update_grid(self) -> None:
         for row in self.rectangles:
-            for item in row:
-                rect, color = item[0], item[1]
-                pygame.draw.rect(self.DISPLAYSURF, color, rect)
+            for rectangle in row:
+                rect_pos_and_dimensions, color = rectangle[0], rectangle[1]
+                pygame.draw.rect(self.DISPLAYSURF, color, 
+                                 rect_pos_and_dimensions)
+
     def renew_grid(self):
         for row in self.rectangles:
-            for item in row:
-                item[1] = self.COLOR_GREEN
-    def new_gen_info(self):
-        return {"start_coords": (0, 0), 
+            for rectangle in row:
+                self.change_color(rectangle, self.COLOR_GREEN)
+
+    def gen_new_info(self):
+        return {"start_coords" : (0, 0), 
                 "goal_coords"  : (0, 0), 
                 "is_start_set" :  False, 
                 "is_goal_set"  :  False}
 
-    def unify_values(self, value) -> tuple:
+    def unify_values(self, value) -> int:
         if value == self.COLOR_BLUE:
             return 2
         if value == self.COLOR_GREEN:
@@ -64,35 +67,44 @@ class AlgoVisualizer:
 
     def calculate_path(self) -> list:
         unified_matrix = self.unify_matrix(self.rectangles)
+        start_algo_time = time.time()
         return compute_a_star(self.general_info["start_coords"], 
                               self.general_info["goal_coords"], 
                               unified_matrix)	
+
+    def change_color(self, rectangle, color) -> None:
+        rectangle[1] = color
 
     def visualize_path(self, path) -> None:
         for x, y in path:
             if ((x, y) != self.general_info["goal_coords"] and
                 (x, y) != self.general_info["start_coords"]):
-                self.rectangles[x][y][1] = self.COLOR_CUSTOM
+                self.change_color(self.rectangles[x][y], self.COLOR_CUSTOM)
+                
 
-    def check_mouse_collision(self, event):
+    def check_mouse_collision(self, event) -> dict:
         for index_x, r_row in enumerate(self.rectangles):
-            for index_y, item in enumerate(r_row):
-                rect = item[0]
-                if rect.collidepoint(event.pos):
-                    return {"num_coords": (index_x, index_y), 
-                            "object": item, 
-                            "has_collided": True}
+            for index_y, rectangle in enumerate(r_row):
+                rect_pos_and_dimensions = rectangle[0]
+                try:
+                    if rect_pos_and_dimensions.collidepoint(event.pos):
+                        return {"num_coords": (index_x, index_y), 
+                                "object": rectangle, 
+                                "has_collided": True}
+                except:
+                    pass
 
-    def check_single_mouse_click(self, event_list, click_info, 
-                                 event, addit_conditional, cords) -> None:
+    def check_single_mouse_click(self, event_list, event, click_info, 
+                                 status_key, coords_key) -> None:
         if event_list[click_info["mouse_button"]]:
             mouse_collision_info = self.check_mouse_collision(event)
             try:
                 if (mouse_collision_info["has_collided"] and 
-                    not self.general_info[addit_conditional]):
-                    mouse_collision_info["object"][1] = click_info["color"]
-                    self.general_info[cords] = mouse_collision_info["num_coords"] 
-                    self.general_info[addit_conditional] = True				
+                    not self.general_info[status_key]):
+                    self.change_color(mouse_collision_info["object"],
+                                      click_info["color"])
+                    self.general_info[coords_key] = mouse_collision_info["num_coords"] 
+                    self.general_info[status_key] = True				
             except:
                 pass
 
@@ -101,19 +113,18 @@ class AlgoVisualizer:
             mouse_collision_info = self.check_mouse_collision(event)
             try:
                 if mouse_collision_info["has_collided"]:
-                    mouse_collision_info["object"][1] = click_info["color"]
+                    self.change_color(mouse_collision_info["object"],
+                                      click_info["color"])
             except:
                 pass			
 
     def listen_mouse_events(self, general_info, event) -> None:
         mouse_buttons_pressed = pygame.mouse.get_pressed()
-        self.check_single_mouse_click(mouse_buttons_pressed, 
+        self.check_single_mouse_click(mouse_buttons_pressed, event,
                                       self.LMB_CLICK_INFO, 
-                                      event, 
                                       "is_start_set", "start_coords")
-        self.check_single_mouse_click(mouse_buttons_pressed, 
-                                      self.MMB_CLICK_INFO, 
-                                      event, 
+        self.check_single_mouse_click(mouse_buttons_pressed, event,
+                                      self.MMB_CLICK_INFO,  
                                       "is_goal_set", "goal_coords")
         self.check_multiple_mouse_clicks(mouse_buttons_pressed, 
                                          self.RMB_CLICK_INFO, 
@@ -128,7 +139,7 @@ class AlgoVisualizer:
                 self.visualize_path(self.calculate_path())
             if event.key == K_BACKSPACE:
                 self.renew_grid()
-                self.general_info = self.new_gen_info()
+                self.general_info = self.gen_new_info()
 
     def main_loop(self) -> None:
         pygame.display.init()
@@ -137,11 +148,11 @@ class AlgoVisualizer:
         pygame.mixer.quit()
         self.create_grid()
         self.update_grid()
-        print(type(pygame.mouse.get_pressed()))
-        print(pygame.mouse.get_pressed())
+
         while True: # main game loop
             events = pygame.event.get()
             for event in events:
+                print(event)
                 self.listen_keyboard_events(event)
                 self.listen_mouse_events(self.general_info, event)
 
